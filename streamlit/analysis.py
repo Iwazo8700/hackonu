c9355d85fd6cec708647f5e81397c7b32a8e12d3
Iwazo8700 <enzo.iwata8700@gmail.com>
2022-09-24 23:59:45 -0300

    Partial feat: Init the analysis file

---
+++ streamlit/analysis.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import sqlite3
+
+DB_PATH = "/db/{}"
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by the db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except Exception as e:
+        st.write(e)
+
+    return conn
+
+
+conn = create_connection(DB_PATH.format("database.sqlite3"))
+conn.execute()
