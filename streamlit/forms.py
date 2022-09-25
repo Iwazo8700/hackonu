238c213e9ca0d676c520190330a291051ef46c2c
Iwazo8700 <enzo.iwata8700@gmail.com>
2022-09-24 23:59:31 -0300

    Fix: Change selectbox position

--- streamlit/forms.py
+++ streamlit/forms.py
@@ -2,9 +2,10 @@ import streamlit as st
 # Using the "with" syntax
 crimes_compliance = ['Assedio moral', 'Assedio sexual', 'Corrupção']
 with st.form(key='my_form'):
-    text_input = st.text_input(label='Relato')
     select_box = st.selectbox(
     'Crime',
     crimes_compliance)
+    text_input = st.text_input(label='Relato')
+
     submit_button = st.form_submit_button(label='Submit')
 
